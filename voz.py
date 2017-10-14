1e1adbc233a1c8c6db03b507ec48f2bad6263be9
Carlos Garcia Saura <carlosgarciasaura@gmail.com>
2017-10-14 22:44:10 +0200

    edited voice

--- voz.py
+++ voz.py
@@ -19,9 +19,9 @@ def romualdo_says(texto):
     #print t
     decir(t)
   
-romualdo_says("Hello, how are you? I love chicken; Oh my gosh! look at that bunny! It's sooo pretty.")
+#romualdo_says("Hello, how are you? I love chicken; Oh my gosh! look at that bunny! It's sooo pretty.")
 
-romualdo_says("We're no strangers to love. You know the rules, and so do I! A full commitment's what I'm thinking of. You wouldn't get this from any other guy! I, just wanna tell you how I'm feeling. Gotta make you understand! Never gonna give you up! Never gonna let you down! Never gonna run around and desert you! Never gonna make you cry! Never gonna say goodbye! Never gonna tell a lie, and hurt you!")
+#romualdo_says("We're no strangers to love. You know the rules, and so do I! A full commitment's what I'm thinking of. You wouldn't get this from any other guy! I, just wanna tell you how I'm feeling. Gotta make you understand! Never gonna give you up! Never gonna let you down! Never gonna run around and desert you! Never gonna make you cry! Never gonna say goodbye! Never gonna tell a lie, and hurt you!")
 
  
  
