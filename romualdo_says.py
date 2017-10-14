888196172e4baf68c278eb0b85ea032643308624
Carlos Garcia Saura <carlosgarciasaura@gmail.com>
2017-10-14 23:47:58 +0200

    Añadido detector de voz / Modificado sintetizador

---
+++ romualdo_says.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import random
+import subprocess
+import re
+
+random.seed()
+
+def decir(texto):
+  tones=[9.19, 20.21, 32.6, 39.38, 54.1, 70.62, 89.19]
+  velocities=[100,110,120,130]
+  # 'setsid -w' removes ALL output from the espeak program
+  subprocess.check_output(['setsid', '-w', 'espeak', '-v', 'mb-us1', '-s', str(random.choice(velocities)), '-p', str(random.choice(tones)), str(texto)])
+
+def romualdo_says(texto):
+  tokens = re.split('(?<=[;,.:!?])\s*', texto)
+  for t in tokens:
+    #print t
+    decir(t)
+  
+#romualdo_says("Hello, how are you? I love chicken; Oh my gosh! look at that bunny! It's sooo pretty.")
+
+#romualdo_says("We're no strangers to love. You know the rules, and so do I! A full commitment's what I'm thinking of. You wouldn't get this from any other guy! I, just wanna tell you how I'm feeling. Gotta make you understand! Never gonna give you up! Never gonna let you down! Never gonna run around and desert you! Never gonna make you cry! Never gonna say goodbye! Never gonna tell a lie, and hurt you!")
+
+ 
+ 
