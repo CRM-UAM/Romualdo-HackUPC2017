43949895c033831c0cf9ad4e3c8cf8bc1d32d6ac
Carlos Garcia Saura <CarlosGS@users.noreply.github.com>
2017-10-14 19:27:37 +0200

    Add files via upload

---
+++ servo_test.py
@@ -0,0 +1,25 @@
+
+import time
+import serial
+
+ser = serial.Serial('/dev/ttyUSB0', 115200)
+time.sleep(2) # wait for servo to initialize
+
+def servo_angle(degrees):
+    if degrees == 0: degrees = 1 # arduino parseInt() limitations don't allow to send 0
+    ser.write(str(degrees)+"\n")
+    ser.flush()
+
+servo_angle(0)
+time.sleep(1)
+
+servo_angle(90)
+time.sleep(1)
+
+servo_angle(180)
+time.sleep(1)
+
+servo_angle(45)
+time.sleep(1)
+
+
