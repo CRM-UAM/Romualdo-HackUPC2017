c8fd40601b57845d02000ed7c055169dbd80f86c
Ignacio Martin <imartinn.ni@gmail.com>
2017-10-14 23:16:50 +0200

    moved mic to root folder

---
+++ eventos.py
@@ -0,0 +1,9 @@
+def veoMovimiento():
+    #TODO:PACOOOOOO
+
+def personaMeAtiende():
+    #TODO: comprobar si la conocemos
+    #Saludar y escuchar
+
+def personaSeVa(persona):
+    #TODO: si la conocemos, adios Paco!
